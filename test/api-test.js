fab7596d900e64d426fbb4ba1382c95a7e814a9d
Fedor Indutny <fedor@indutny.com>
2015-05-22 00:46:43 +0200

    lib: .verify(), add tests

--- test/api-test.js
+++ test/api-test.js
@@ -2,6 +2,7 @@ var ocsp = require('../');
 var fixtures = require('./fixtures');
 
 var assert = require('assert');
+var https = require('https');
 
 describe('OCSP Stapling Provider', function() {
   var cert = null;
@@ -19,16 +20,50 @@ describe('OCSP Stapling Provider', function() {
     o = new ocsp.Cache();
   });
 
-  it('should validate google.com', function(cb) {
-    ocsp.check({
-      cert: fixtures.google,
-      issuer: fixtures.googleIssuer
-    }, function(err, res) {
-      if (err)
-        throw err;
+  describe('.check()', function() {
+    it('should validate google.com', function(cb) {
+      ocsp.check({
+        cert: fixtures.google,
+        issuer: fixtures.googleIssuer
+      }, function(err, res) {
+        if (err)
+          throw err;
 
-      assert.equal(res.type, 'good');
-      cb();
+        assert.equal(res.type, 'good');
+        cb();
+      });
+    });
+  });
+
+  describe('.verify()', function() {
+    it('should verify reddit.com\'s stapling', function(cb) {
+      var req = https.request({
+        host: 'reddit.com',
+        port: 443,
+        requestOCSP: true
+      }, function(res) {
+        // Should not be called
+        assert(false);
+      });
+
+      req.on('socket', function(socket) {
+        socket.on('OCSPResponse', function(stapling) {
+          var cert = socket.getPeerCertificate(true);
+
+          var req = ocsp.request.generate(cert.raw, cert.issuerCertificate.raw);
+          assert.doesNotThrow(function() {
+
+            var res = ocsp.verify({
+              request: req,
+              response: stapling
+            });
+
+            assert.equal(res.type, 'good');
+            socket.destroy();
+            cb();
+          });
+        });
+      });
     });
   });
 });
